b31d11eda9a6594aacebf948e932e83506fcb791
mxmule <maheshsmule12@gmail.com>
2025-01-09 21:41:56 +0530

    motion ui installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React, { useState } from 'react';
+import { Magnetic } from './components/magnetic';
+
 import './App.scss'
 
 const App: React.FC = () => {
- return <p>Project</p>
+    return <div className='w-full h-screen flex justify-center items-center bg-white'>
+        <Magnetic>
+        <button
+            type='button'
+            className='inline-flex items-center rounded-md border border-black bg-transparent px-4 py-2 text-lg text-zinc-950 transition-all duration-300 hover:bg-zinc-100 dark:border-zinc-900 dark:bg-transparent dark:text-zinc-50 dark:hover:bg-zinc-600'
+        >
+            <span>Submit</span>
+        </button>
+    </Magnetic>
+    </div>
 };
 
 export default App;
