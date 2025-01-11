73a4edf052e6d59c0e25664d58880bb64928d198
mxmule <maheshsmule12@gmail.com>
2025-01-11 21:43:59 +0530

    banner

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import banner from '../../assets/banner.jpg';
 
 export const Banner: React.FC = () => {
     return (
         <div className="banner">
             <h1>Welcome to My Portfolio</h1>
             <p>This is a showcase of my work and projects.</p>
+            <img src={banner} alt="banner" />
         </div>
     );
 };
